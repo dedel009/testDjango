b210b1ddd985778b7133731f509be5e949df2651
kai <m01056994380@gmail.com>
2024-06-17 18:37:30 +0900

    settings.py 업데이트

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+#세팅
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
