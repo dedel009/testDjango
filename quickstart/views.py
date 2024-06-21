cb21278de7e8f26cfa82f7a5abe22aaa745d5716
kai <m01056994380@gmail.com>
2024-06-21 19:19:05 +0900

    reponse 추가 및 함수형 view 추가

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User, Group
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 from quickstart.models import Snippet
@@ -27,48 +29,46 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-@csrf_exempt
-def snippet_list(request):
+@api_view(['GET', 'POST'])  #  api_view 데코레이터를 이용하여 지원하는 메소드를 지정
+def snippet_list(request, format=None):
     """
     snippets의 모든 목록을 조회하거나 새로운 snippet을 생성하는 함수
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(reqeust, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(reqeust, pk, format=None):
     """
     snippet 조회, 업데이트, 삭제 함수
     """
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if reqeust.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif reqeust.method == 'PUT':
-        data = JSONParser().parse(reqeust)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=reqeust.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif reqeust.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
