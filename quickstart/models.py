d3044e4f6029de943d55afbb44d5ec2eb0d96fdc
kai <m01056994380@gmail.com>
2024-06-25 16:53:40 +0900

    models update

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 # Create your models here.
 
@@ -16,7 +20,23 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey(User, related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     class Meta:    # 모델의 정보를 담고 있는 클래스로 권한, 데이터베이스 이름, 추상화, 순서 지정과 같은걸 할 수 있음
         ordering = ['created']
+
+
+def save(self, *args, **kwargs):
+    """
+    Use the `pygments` library to create a highlighted HTML
+    representation of the code snippet.
+    """
+    lexer = get_lexer_by_name(self.language)
+    linenos = 'table' if self.linenos else False
+    options = {'title': self.title} if self.title else {}
+    formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                              full=True, **options)
+    self.highlighted = highlight(self.code, lexer, formatter)
+    super(Snippet, self).save(*args, **kwargs)
 
--- quickstart/views.py
+++ quickstart/views.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import User, Group
-from django.http import JsonResponse, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
