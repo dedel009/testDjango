d91e5890233348d23ec3997dc2c33100c469eb9a
kai <m01056994380@gmail.com>
2024-06-27 18:52:11 +0900

    add testapi app

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -4,7 +4,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from quickstart import views
 
 urlpatterns = [
-    path('', views.api_root),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
